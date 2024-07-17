c18c831354e7541d082fdcd90d0fbe8b5f3273af
William Samuelson <william.samuelson@gmail.com>
2024-07-17 14:31:46 +0200

    add support for any number of Majoranas in basis

--- src/majorana_basis.jl
+++ src/majorana_basis.jl
@@ -26,9 +26,8 @@ end
 
 All combinations of many body Majorana labels up to products of max_combinations from single particle Majorana labels.
 """
-function full_many_body_majorana_labels(single_particle_labels, max_combinations)
-    lengths = 1:2:max_combinations
-    mapreduce(length->collect(combinations(single_particle_labels, length)), vcat, lengths)
+function full_many_body_majorana_labels(single_particle_labels, combination_lengths::AbstractVector)
+    mapreduce(length->collect(combinations(single_particle_labels, length)), vcat, combination_lengths)
 end
 
 """
@@ -46,20 +45,42 @@ Constructs a many body Majorana basis from a single particle basis.
 """
 struct ManyBodyMajoranaBasis{M}<:AbstractMajoranaBasis
     dict::M
-    function ManyBodyMajoranaBasis(γ::SingleParticleMajoranaBasis, max_combinations=length(γ))
-        many_body_labels = full_many_body_majorana_labels(labels(γ), max_combinations)
+    function ManyBodyMajoranaBasis(γ::SingleParticleMajoranaBasis, combination_lengths::AbstractVector)
+        many_body_labels = full_many_body_majorana_labels(labels(γ), combination_lengths)
         γmb = map(labels->labels_to_manybody_majorana(labels, γ), many_body_labels)
         d = QuantumDots.dictionary(zip(many_body_labels, γmb))
         new{typeof(d)}(d)
     end
 end
 
+function ManyBodyMajoranaBasis(γ::SingleParticleMajoranaBasis, max_combination_length::Int=length(γ))
+    return ManyBodyMajoranaBasis(γ, 1:2:max_combination_length)
+end
+
 """
 Constructor that takes a FermionBasis instead.
 """
-function ManyBodyMajoranaBasis(fermion_basis::FermionBasis, max_combinations=2*nbr_of_fermions(fermion_basis), majorana_labels=(:+,:-))
+function ManyBodyMajoranaBasis(fermion_basis::FermionBasis, combination_lengths::AbstractVector, majorana_labels=(:+,:-))
     γ = SingleParticleMajoranaBasis(fermion_basis, majorana_labels)
-    return ManyBodyMajoranaBasis(γ, max_combinations)
+    return ManyBodyMajoranaBasis(γ, combination_lengths)
+end
+
+function ManyBodyMajoranaBasis(fermion_basis::FermionBasis, max_combination_length::Int=nbr_of_majoranas(fermion_basis), majorana_labels=(:+,:-))
+    return ManyBodyMajoranaBasis(fermion_basis, 1:2:max_combination_length, majorana_labels)
+end
+
+@testitem "ManyBodyMajoranaBasis constructor" begin
+    using QuantumDots
+    c = FermionBasis(1:3)
+    γ_sp = SingleParticleMajoranaBasis(c)
+    γ_mb = ManyBodyMajoranaBasis(c, 1)
+    @test γ_sp.dict.values == γ_mb.dict.values
+    @test γ_sp.dict.values == ManyBodyMajoranaBasis(γ_sp, 1).dict.values
+    γ3 = ManyBodyMajoranaBasis(c, 3:3)
+    @test all([Majoranas.nbr_of_sp_majoranas(γ3, i) == 3 for i in 1:length(γ3)])
+    γ_new_label = ManyBodyMajoranaBasis(c, 3, (:x, :y))
+    sp_labels = [label for labelvec in labels(γ_new_label) for label in labelvec]
+    @test all([any((:x, :y) .== label[end]) for label in sp_labels])
 end
 
 ### with SPMajoranas, use either tuple with the label or several arguments to index
